8699eb8e0c7483d3330e09e1a2556b98a6054cc5
bombin78 <bombin78@mail.ru>
2019-08-14 20:36:53 +0300

    создание компонента "InputNumber"

---
+++ class-work/lesson-9/state-managment/src/components/InputNumber.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="input-number">
+        <button @click="dec">-</button>
+        <!-- <input :value="realValue" type="text"> -->
+        <input v-model="realValue" type="text">
+        <button @click="inc">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'InputNumber',
+        props: {
+            value: {
+                type: Number,
+                default: 0,
+            },
+        },
+        data() {
+            return {
+                // копируем входные значения в собственный state
+                realValue: this.value,
+            };
+        },
+        methods: {
+            inc() {
+                this.realValue++;
+                //this.$emit('updated', this.realValue);
+                //this.$emit('input', this.realValue);
+            },
+            dec() {
+                this.realValue--;
+                //this.$emit('updated', this.realValue);
+                //this.$emit('input', this.realValue);
+            },
+        },
+        watch: {
+            realValue(newValue) {
+              this.$emit('input', newValue);
+            },
+        },
+    }
+</script>
--- class-work/lesson-9/state-managment/src/views/About.vue
+++ class-work/lesson-9/state-managment/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
+<div class="about">
     <h1>This is an about page</h1>
-  </div>
+    <!-- <input-number @updated="a = $event" :value="a" />
+    <input-number @updated="b = $event" :value="b" /> -->
+    <input-number v-model="a" />
+    <input-number v-model="b" />
+    <!-- <button @click="add">Сложить</button> -->
+    <p>Результат: {{ result }}</p>
+</div>
 </template>
+<script>
+import InputNumber from '../components/InputNumber.vue';
+export default {
+  components: {InputNumber},
+  data() {
+    return {
+      //result: 0,
+      a: 1,
+      b: 3,
+    };
+  },
+  computed: {
+    result() {
+      return this.a + this.b;
+    },
+  },
+  // methods: {
+  //   add(){
+  //     this.result = this.a + this.b;
+  //   },
+  // },
+}
+</script>
