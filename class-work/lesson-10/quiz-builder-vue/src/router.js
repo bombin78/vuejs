e0da3f350fe85e37e7bfbd3e6e15de270527db5a
bombin78 <bombin78@mail.ru>
2019-08-16 19:43:59 +0300

    page 404 + title

--- class-work/lesson-10/quiz-builder-vue/src/router.js
+++ class-work/lesson-10/quiz-builder-vue/src/router.js
@@ -12,23 +12,49 @@ const router = new Router({
       path: "/",
       name: "home",
       component: Home,
+      meta: {
+          title: "Главная страница конструктора",
+      },
     },
     {
       path: "/about",
       name: "about",
       component: () => import(/* webpackChunkName: "about" */ "./views/About.vue"),
+      meta: {
+          title: "Справочник",
+      },
     },
     {
       path: "/categories",
       name: "categories",
       component: () => import(/* webpackChunkName: "categories" */ "./views/Categories.vue"),
+      meta: {
+          title: "Категории тестов",
+      },
     },
     {
       path: "/quiz",
       name: "quiz",
       component: () => import(/* webpackChunkName: "quiz" */ "./views/Quiz.vue"),
+      meta: {
+          title: "Добавление теста",
+      },
+    },
+    {
+      path: "*", // * - совпадает с любым URL, с которым не было совпадения выше
+      name: "not-found",
+      component: () => import(/* webpackChunkName: "quiz" */ "./views/404.vue"),
+      meta: {
+          title: "Страница не найдена",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+    // не будет работать для SSR
+    document.title = to.meta.title;
+    next();
 });
 
 export default router;
