7a1ef3a4bb042a49d78a9a38ca4d832314d91976
bombin78 <bombin78@mail.ru>
2019-08-23 20:56:00 +0300

    lesson 404 pages

---
+++ class-work/lesson-14/nuxt-demo/pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="container">
+  <h1>О компании</h1>
+  <p>Тут какое-то описание</p>
+</div>
+</template>
---
+++ class-work/lesson-14/nuxt-demo/pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="container">
+  <h1>Контакты</h1>
+  <p>Тут адреса и телефоны</p>
+</div>
+</template>
